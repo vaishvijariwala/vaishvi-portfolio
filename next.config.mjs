95ec53623a64b9395276002e8daba74e8e2c12ef
vaishvi jariwala <vaishvijariwala03@gmail.com>
2026-03-05 11:04:16 -0600

    Changes in the Config file

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/vaishvi-portfolio",
+  assetPrefix: "/vaishvi-portfolio/",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
